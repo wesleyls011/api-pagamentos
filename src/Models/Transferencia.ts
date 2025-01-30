b7e25124f9da6068c28e205b509efeec5add7f79
Wesley Sanches <wesleysanches27@outlook.com>
2025-01-29 22:42:17 -0300

    new model transferencia

---
+++ src/Models/Transferencia.ts
@@ -0,0 +1,72 @@
+import {Model, DataTypes} from 'sequelize';
+import sequelize from '../config/database';
+import { Usuario } from './Usuario';
+import { Lojista } from './Lojista';
+
+export class Transferencia extends Model {
+    public readonly id!: number;
+    public valor!: number;
+    public data!: Date;
+    public usuarioId!: number;
+    public lojistaId!: number;
+    public status!: string;
+}
+
+Transferencia.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        valor: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            validate: {
+                min: 0
+            }
+        },
+        data: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        usuarioId: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            references: {
+                model: Usuario,
+                key: 'id'
+            }
+        },
+        lojistaId: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            references: {
+                model: Lojista,
+                key: 'id'
+            }
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isIn: [['concluida', 'pendente', 'cancelada']]
+            }
+        }
+    },
+
+    {
+        sequelize,
+        tableName: 'Transferencias',
+        timestamps: true
+    }
+
+    
+);
+
+Transferencia.belongsTo(Usuario, {foreignKey: 'usuarioId', onDelete: 'CASCADE'});
+Transferencia.belongsTo(Lojista, {foreignKey: 'lojistaId', onDelete: 'CASCADE'});
+
+Usuario.hasMany(Transferencia, {foreignKey: 'usuarioId'});
+Lojista.hasMany(Transferencia, {foreignKey: 'lojistaId'});
