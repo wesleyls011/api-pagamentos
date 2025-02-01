83f2e8453611e0e7efe718e3fd13153f8d3728f1
Wesley Sanches <wesleysanches27@outlook.com>
2025-01-31 23:40:01 -0300

    user service and lojista service

---
+++ src/Services/lojistaService.ts
@@ -0,0 +1,44 @@
+import { Lojista } from "../Models/Lojista";
+import { Response, Request } from "express";
+import { CriptografarSenha } from "../Models/CriptografarSenha";
+
+class LojistaService {
+
+    async createLojista(req: Request, res: Response){
+        const {nomeCompleto, CNPJ, email, senha} = req.body;
+
+        try{
+
+            const criptografarSenha = new CriptografarSenha();
+            await criptografarSenha.criptografarSenha(senha);
+
+            const lojista = await Lojista.create({
+                nomeCompleto,
+                CNPJ,
+                email,
+                senha: criptografarSenha.senhaCriptografada
+            });
+
+            return res.status(201).json(lojista);
+
+        } catch (error: any){
+            if (error.name === "SequelizeUniqueConstraintError"){
+                res.status(400).json({message: 'email ou CNPJ ja cadastrado'});
+            } else {
+                console.error(error);
+                res.status(500).json({message: "Erro interno"});
+            }
+        }
+    }
+
+    async getLojistaById(id: number){
+        const lojista = await Lojista.findByPk(id);
+        if(!lojista){
+            throw new Error('Lojista nao encontrado');
+        }
+
+        return lojista;
+    }
+}
+
+export default new LojistaService();
