aa3d9b3bb34439cd26a79b6861cba851d77a174c
Wesley Sanches <wesleysanches27@outlook.com>
2025-02-01 12:51:19 -0300

    update user controller

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -1,27 +1,52 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import userService from "../Services/userService";
 
-class userController{
+class userController {
 
-    async createUser(req: Request, res: Response){
-        try{
-            const user = await userService.createUsuario(req,res);
+    async createUser(req: Request, res: Response) {
+        try {
+            const user = await userService.createUsuario(req, res);
             return res.status(201).json(user);
-        } catch(error){
-            return res.status(500).json({message: "Erro ao criara usuario"});
+        } catch (error) {
+            return res.status(500).json({ message: "Erro ao criara usuario" });
         }
     }
 
-    async getUserById(req: Request, res: Response){
-        const {id} = req.params;
+    async getUserById(req: Request, res: Response) {
+        const { id } = req.params;
 
-        try{
+        try {
             const user = await userService.getUsuarioById(Number(id));
             return res.status(200).json(user);
-        } catch(error){
-            return res.status(404).json({message: "Usuario nao encontrado"});
+        } catch (error) {
+            return res.status(404).json({ message: "Usuario nao encontrado" });
         }
-}
+    }
+
+    async updateUser(req: Request, res: Response) {
+        const { id } = req.params;
+        const dadosAtualizados = req.body;
+
+        try {
+            const userAtualizado = await userService.updateUsuario(Number(id), dadosAtualizados);
+            return res.status(200).json(userAtualizado);
+        } catch (error) {
+            return res.status(404).json({ message: "Usuario nao encontrado" });
+        }
+    }
+
+    async deleteUser(req: Request, res: Response) {
+        const { id } = req.params;
+
+        try {
+            const userDeletado = await userService.deleteUsuario(Number(id));
+            return res.status(200).json(userDeletado);
+        } catch (error) {
+            return res.status(404).json({ message: "Usuario nao encontrado" });
+        }
+    }
+
+
 }
 
 export default new userController();
